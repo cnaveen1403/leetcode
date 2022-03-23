aa7d90b10a4cc17892d6747d62893b1091072c36
Naveen Kumar <c.naveen1403@gmail.com>
2022-03-23 23:11:11 +0530

    remove Element solved

---
+++ practice/removeElements.js
@@ -0,0 +1,21 @@
+/**
+ * @param {number[]} nums
+ * @param {number} val
+ * @return {number}
+ */
+
+ var nums = [0,1,2,2,3,0,4,2], val = 2;
+
+ var removeElement = function(nums, val) {
+    for(var i =0; i < nums.length; i++){
+        if(nums[i] == val){
+            nums.splice(i, 1);
+            i--;
+        }
+            
+    }
+
+    return nums;
+};
+
+console.log(removeElement(nums, val));
